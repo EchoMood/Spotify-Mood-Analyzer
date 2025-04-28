0f27dc6f1aeb473d36303f6ada88e0e962b8909f
thepreetprasad <23857493@student.uwa.edu.au>
2025-04-29 04:08:31 +0800

    App Changes

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/visualise')
 def visualise():
     return render_template('visualise.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/share')
+def share():
+    return render_template('share.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
